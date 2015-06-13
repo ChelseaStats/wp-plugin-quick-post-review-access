bf11681e3360f547d9cc9c79f6c106e1adeeba64
ChelseaStats <git@thechels.co.uk>
2015-06-13 23:32:29 +0100

    fix url

--- trunk/Quick_Review_Post_Access.php
+++ trunk/Quick_Review_Post_Access.php
@@ -7,27 +7,12 @@
 /*
 Plugin Name: Quick Review Post Access
 Version: 1.3.4
-Plugin URI: http://thecellarroom.co.uk
+Plugin URI: http://thecellarroom.uk
 Author: The Cellar Room Limited
-Author URI: http://www.thecellarroom.co.uk
+Author URI: http://thecellarroom.uk
 Description: Adds a link to 'Pending', 'future' (scheduled) and 'Drafts' under the Posts, Pages, and other custom post type sections in the admin menu. Compatible with WordPress 3.0+.
+Copyright (c) 2015 by The Cellar Room Limited
 
-*/
-
-/*
-Copyright (c) 2012 by The Cellar Room Limited
-
-Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation
-files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy,
-modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the
-Software is furnished to do so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
-OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
-LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR
-IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 */
 
 if ( is_admin() && !class_exists( 'TCR_QuickDraftsAccess' ) ) :
